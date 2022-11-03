71a9587767914d9e277bc7e2ffa7ff9a0577b8f2
swimdroid <65006945+SwimPaste@users.noreply.github.com>
2022-11-03 21:47:17 +0900

    Update check.php

--- check.php
+++ check.php
@@ -15,28 +15,27 @@ $hostname = gethostbyaddr($_SERVER['REMOTE_ADDR']);
 $fh = fopen('logs.txt', 'a'); 
 fwrite($fh, ''."".$ip ."\n");
 $keys = array(
-   "yDQPEDE60PALQo74rZsLck3ecipPuvHa", //no use
-   "z9QOWqONHsoC4ARPxbbQjym9qE7vmSYv", //no use
-   "MXpg82MSzmERnJaVIdVroKFTJUBmCOZE", //no use
-   "94Jo1LIKEn6f2STh8f6NZpITmjWCiJfi", //no use
-   "kJAXed45qNbT0SqoH1TjFgj3CamlbvVJ", //no use
-   "2DUm6sCBrUHqDGz0FmvPIcTc4IU2s483", //no use
-   "CCAdiIpFg1goYiHKw5jeAEXGGTMZuyYk", //no use
-   "eTJCQKmTDKZ2uNzcXeJg9AWlbdVBIDRC", //no use
-   "ywu3bgLpuNenAzn22amYRwPF7JGk4sC1", //no use
-   "nw1xUIcOvgqOZT12HkyFLsyJMA62iHv5", //no use
-   "3ARPfNQd3xGhicFYGBoDPVtDHqUe9DiS", //no use
-   "miTZ2YpTXHrSUd5z8lLdG69HlZKy6NFH", //no use
-   "CKs5X64uoIVLAb5lpcJha9XZgZyKGsN1", //no use
-   "Xdwhd5TCS7P5y1aUmh35c1mWk1iUXUQi", //no use
-   "u6xhPy5zYlXiPhEjFDxYis1LOyu2MJaE", //no use
-   "nGPDyNJYcpxE0lRsCJhIYtTI4p0x1iKS", //no use
-//16 in total
-); 
-$sub = $_GET["key"];
+"yDQPEDE60PALQo74rZsLck3ecipPuvHa",//nouse
+"z9QOWqONHsoC4ARPxbbQjym9qE7vmSYv",//nouse
+"MXpg82MSzmERnJaVIdVroKFTJUBmCOZE",//nouse
+"94Jo1LIKEn6f2STh8f6NZpITmjWCiJfi",//nouse
+"kJAXed45qNbT0SqoH1TjFgj3CamlbvVJ",//nouse
+"2DUm6sCBrUHqDGz0FmvPIcTc4IU2s483",//nouse
+"CCAdiIpFg1goYiHKw5jeAEXGGTMZuyYk",//nouse
+"eTJCQKmTDKZ2uNzcXeJg9AWlbdVBIDRC",//nouse
+"ywu3bgLpuNenAzn22amYRwPF7JGk4sC1",//nouse
+"nw1xUIcOvgqOZT12HkyFLsyJMA62iHv5",//nouse
+"3ARPfNQd3xGhicFYGBoDPVtDHqUe9DiS",//nouse
+"miTZ2YpTXHrSUd5z8lLdG69HlZKy6NFH",//nouse
+"CKs5X64uoIVLAb5lpcJha9XZgZyKGsN1",//nouse
+"Xdwhd5TCS7P5y1aUmh35c1mWk1iUXUQi",//nouse
+"u6xhPy5zYlXiPhEjFDxYis1LOyu2MJaE",//nouse
+"nGPDyNJYcpxE0lRsCJhIYtTI4p0x1iKS"//nouse
+);
+$sub= $_GET["key"];
 if (in_array($sub,$keys,TRUE)) {
-    echo "заходи❤"; 
+    echo "Whitelisted"; 
 } else {
-    echo "зачем ты попросил у людей этот скрипт или ты просто дебил"; 
+    echo "Not Whitelisted"; 
 }
 ?>
