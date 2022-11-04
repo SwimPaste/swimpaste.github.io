49e1cbde1b2901723a6056d5396f343ccc1a2ca2
swimdroid <65006945+SwimPaste@users.noreply.github.com>
2022-11-04 12:30:55 +0900

    Update check.php

--- check.php
+++ check.php
@@ -15,22 +15,22 @@ $hostname = gethostbyaddr($_SERVER['REMOTE_ADDR']);
 $fh = fopen('logs.txt', 'a'); 
 fwrite($fh, ''."".$ip ."\n");
 $keys = array(
-"yDQPEDE60PALQo74rZsLck3ecipPuvHa",//nouse
-"z9QOWqONHsoC4ARPxbbQjym9qE7vmSYv",//nouse
-"MXpg82MSzmERnJaVIdVroKFTJUBmCOZE",//nouse
-"94Jo1LIKEn6f2STh8f6NZpITmjWCiJfi",//nouse
-"kJAXed45qNbT0SqoH1TjFgj3CamlbvVJ",//nouse
-"2DUm6sCBrUHqDGz0FmvPIcTc4IU2s483",//nouse
-"CCAdiIpFg1goYiHKw5jeAEXGGTMZuyYk",//nouse
-"eTJCQKmTDKZ2uNzcXeJg9AWlbdVBIDRC",//nouse
-"ywu3bgLpuNenAzn22amYRwPF7JGk4sC1",//nouse
-"nw1xUIcOvgqOZT12HkyFLsyJMA62iHv5",//nouse
-"3ARPfNQd3xGhicFYGBoDPVtDHqUe9DiS",//nouse
-"miTZ2YpTXHrSUd5z8lLdG69HlZKy6NFH",//nouse
-"CKs5X64uoIVLAb5lpcJha9XZgZyKGsN1",//nouse
-"Xdwhd5TCS7P5y1aUmh35c1mWk1iUXUQi",//nouse
-"u6xhPy5zYlXiPhEjFDxYis1LOyu2MJaE",//nouse
-"nGPDyNJYcpxE0lRsCJhIYtTI4p0x1iKS"//nouse
+"yDQPEDE60PALQo74rZsLck3ecipPuvHa",
+"z9QOWqONHsoC4ARPxbbQjym9qE7vmSYv",
+"MXpg82MSzmERnJaVIdVroKFTJUBmCOZE",
+"94Jo1LIKEn6f2STh8f6NZpITmjWCiJfi",
+"kJAXed45qNbT0SqoH1TjFgj3CamlbvVJ",
+"2DUm6sCBrUHqDGz0FmvPIcTc4IU2s483",
+"CCAdiIpFg1goYiHKw5jeAEXGGTMZuyYk",
+"eTJCQKmTDKZ2uNzcXeJg9AWlbdVBIDRC",
+"ywu3bgLpuNenAzn22amYRwPF7JGk4sC1",
+"nw1xUIcOvgqOZT12HkyFLsyJMA62iHv5",
+"3ARPfNQd3xGhicFYGBoDPVtDHqUe9DiS",
+"miTZ2YpTXHrSUd5z8lLdG69HlZKy6NFH",
+"CKs5X64uoIVLAb5lpcJha9XZgZyKGsN1",
+"Xdwhd5TCS7P5y1aUmh35c1mWk1iUXUQi",
+"u6xhPy5zYlXiPhEjFDxYis1LOyu2MJaE",
+"nGPDyNJYcpxE0lRsCJhIYtTI4p0x1iKS"
 );
 $sub= $_GET["key"];
 if (in_array($sub,$keys,TRUE)) {
